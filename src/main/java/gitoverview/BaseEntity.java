f2a953dbaf3759b6476daa36d90e3ec0f0dfc0d3
Andrey Kuzmich <doc211b@mail.ru>
2020-12-10 10:02:51 +0300

    Initital commit of the project

---
+++ src/main/java/gitoverview/BaseEntity.java
@@ -0,0 +1,24 @@
+package gitoverview;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
